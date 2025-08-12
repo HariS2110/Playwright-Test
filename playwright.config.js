2b7710f1453c4d84d5b556097c13057d48832fd0
Hari Soni <harisoni535@gmail.com>
2025-08-12 21:21:32 +0100

    Update

--- playwright.config.js
+++ playwright.config.js
@@ -26,11 +26,12 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    video: 'on', // Record all tests
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure', // 'on-first-retry' to collect trace only on the first retry
     screenshot: 'only-on-failure',
   },
 
